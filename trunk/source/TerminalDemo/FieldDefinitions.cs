21e9653c436c2d5a378c994c3f9c0457c1e60033
Skwerl_cp <SND\Skwerl_cp>
2013-08-09 10:26:39 -0700

    Adding Terminal Demo project to source control

---
+++ trunk/source/TerminalDemo/FieldDefinitions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalDemo
+{
+	public class FieldIndices
+	{
+
+		public static class Login
+		{
+			public static int Username = 18;
+			public static int GroupID = 21;
+			public static int Password = 25;
+			public static int Language = 29;
+			public static int NewPassword = 34;
+		}
+		public static class Lord
+		{
+			public static int Command = 1;
+			public static int Function = 5;
+			public static int Item = 10;
+			public static int Action = 12;
+			public static int Quantity = 15;
+			public static int Price = 17;
+			public static int Market = 19;
+			public static int Dealer = 21;
+			public static int SpecTyping = 25;
+			public static int NT = 27;
+			public static int PotcSym = 29;
+			public static int Account = 31;
+			public static int SBDT = 33;
+			public static int InvAC = 35;
+			public static int MessageFlr = 39;
+			public static int SecNo = 42;
+			public static int BrSeq = 44;
+			public static int BrSeq2 = 45;
+			public static int Conc = 47;
+		}
+	}
+
+	public class ScreenFieldLocation
+	{
+		public int Index { get; set; }
+		public int Row { get; set; }
+		public int Column { get; set; }
+		public int Length { get; set; }
+
+		public ScreenFieldLocation(int row, int column, int length)
+		{
+			this.Row = row;
+			this.Column = column;
+			this.Length = length;
+		}
+	}
+}
---
+++ trunk/source/TerminalDemo/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TerminalDemo
+{
+
+	public partial class MainWindow : Window
+	{
+
+		public Terminal Terminal
+		{
+			get { return (this.Resources["term"] as Terminal); }
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+
+		//This command isn't used in the demo, but can be used when you want to send some predefined text.
+		#region SendText Command
+
+		public static RoutedUICommand SendText = new RoutedUICommand();
+
+		void CanExecuteSendText(object sender, CanExecuteRoutedEventArgs args)
+		{
+			if (true)
+			{
+				args.CanExecute = true;
+			}
+			else
+			{
+				//args.CanExecute = false;
+			}
+		}
+
+
+		void ExecuteSendText(object sender, ExecutedRoutedEventArgs args)
+		{
+			
+		}
+
+		#endregion SendText Command
+
+
+		#region SendCommand Command
+
+		public static RoutedUICommand SendCommand = new RoutedUICommand();
+
+		void CanExecuteSendCommand(object sender, CanExecuteRoutedEventArgs args)
+		{
+			args.CanExecute = this.Terminal.IsConnected;
+		}
+
+
+		void ExecuteSendCommand(object sender, ExecutedRoutedEventArgs args)
+		{
+			this.Terminal.SendKey((string)args.Parameter);
+		}
+
+		#endregion SendCommand Command
+
+
+		#region Connect Command
+
+		public static RoutedUICommand Connect = new RoutedUICommand();
+
+		void CanExecuteConnect(object sender, CanExecuteRoutedEventArgs args)
+		{
+			args.CanExecute = !this.Terminal.IsConnected && !this.Terminal.IsConnecting;
+		}
+
+
+		void ExecuteConnect(object sender, ExecutedRoutedEventArgs args)
+		{
+			this.Terminal.Connect();
+		}
+
+		#endregion Connect Command
+
+
+		#region Refresh Command
+
+		public static RoutedUICommand Refresh = new RoutedUICommand();
+
+		void CanExecuteRefresh(object sender, CanExecuteRoutedEventArgs args)
+		{
+			args.CanExecute = this.Terminal.IsConnected;
+		}
+
+
+		void ExecuteRefresh(object sender, ExecutedRoutedEventArgs args)
+		{
+			this.Terminal.Refresh();
+		}
+
+		#endregion Refresh Command
+
+
+		#region DumpFields Command
+
+		public static RoutedUICommand DumpFields = new RoutedUICommand();
+
+		void CanExecuteDumpFields(object sender, CanExecuteRoutedEventArgs args)
+		{
+			args.CanExecute = this.Terminal.IsConnected;
+		}
+
+
+		void ExecuteDumpFields(object sender, ExecutedRoutedEventArgs args)
+		{
+			this.Terminal.DumpFillableFields();
+		}
+
+		#endregion DumpFields Command
+
+
+
+		private void Window_TextInput(object sender, TextCompositionEventArgs e)
+		{
+			if (this.Terminal.IsConnected)
+			{
+				this.Terminal.SendText(e.Text);
+			}
+
+		}
+	
+		
+	}
+}
